1aff9667e4613560b3aea59f3b8625187852bea0
ericzhang08 <yuzhangfish@163.com>
2020-06-13 11:33:45 +0800

    refactor: Ticket should equal by UUID

--- src/main/java/locker/tdd201/Ticket.java
+++ src/main/java/locker/tdd201/Ticket.java
@@ -1,4 +1,25 @@
 package locker.tdd201;
 
+import java.util.Objects;
+import java.util.UUID;
+
 public class Ticket {
+    private UUID uuid;
+
+    public Ticket() {
+        this.uuid = UUID.randomUUID();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ticket ticket = (Ticket) o;
+        return Objects.equals(uuid, ticket.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(uuid);
+    }
 }
