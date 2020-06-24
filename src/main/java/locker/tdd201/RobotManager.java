6dd13b2956f17ddb49d76cda9ffdb3b7cfe9e1e1
ericzhang08 <yuzhangfish@163.com>
2020-06-24 20:28:06 +0800

    should_store_in_first_locker_and_get_ticket_when_store_given_two_not_full_lockers

--- src/main/java/locker/tdd201/RobotManager.java
+++ src/main/java/locker/tdd201/RobotManager.java
@@ -1,15 +1,24 @@
 package locker.tdd201;
 
 import java.util.List;
+import java.util.Optional;
 
 public class RobotManager {
     private List<LockerRobot> robotRepository;
+    private List<Locker> lockerRepository;
 
-    public RobotManager(List<LockerRobot> robots) {
+    public RobotManager(List<LockerRobot> robots, List<Locker> lockers) {
         this.robotRepository = robots;
+        this.lockerRepository = lockers;
     }
 
+
+
     public Ticket store(Bag bag) {
-        return robotRepository.stream().filter(lockerRobot -> !lockerRobot.isFull()).findFirst().get().store(bag);
+        Optional<LockerRobot> first = robotRepository.stream().filter(lockerRobot -> !lockerRobot.isFull()).findFirst();
+        if(first.isPresent()){
+            return first.get().store(bag);
+        }
+        return lockerRepository.stream().filter(locker -> !locker.isFull()).findFirst().get().store(bag);
     }
 }
--- src/test/java/locker/tdd201/LockerRobotManagerTest.java
+++ src/test/java/locker/tdd201/LockerRobotManagerTest.java
@@ -5,14 +5,16 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class LockerRobotManagerTest {
     @Test
     void should_store_in_first_robot_and_get_ticket_when_store_given_two_not_full_robot() {
         SmartLockerRobot smartLockerRobot = new SmartLockerRobot(Arrays.asList(new Locker(1)));
-        RobotManager robotManager = new RobotManager(Arrays.asList(smartLockerRobot));
+        RobotManager robotManager = new RobotManager(Arrays.asList(smartLockerRobot), Arrays.asList());
         Bag bag = new Bag();
         Ticket ticket = robotManager.store(bag);
+        assertNotNull(ticket);
         assertEquals(smartLockerRobot.pickUp(ticket), bag);
     }
     @Test
@@ -20,10 +22,22 @@ public class LockerRobotManagerTest {
         SmartLockerRobot smartLockerRobot = new SmartLockerRobot(Arrays.asList(new Locker(1)));
         smartLockerRobot.store(new Bag());
         PrimaryLockerRobot primaryLockerRobot = new PrimaryLockerRobot(Arrays.asList(new Locker(1)));
-        RobotManager robotManager = new RobotManager(Arrays.asList(smartLockerRobot, primaryLockerRobot));
+        RobotManager robotManager = new RobotManager(Arrays.asList(smartLockerRobot, primaryLockerRobot), Arrays.asList());
 
         Bag bag = new Bag();
         Ticket ticket = robotManager.store(bag);
+        assertNotNull(ticket);
         assertEquals(primaryLockerRobot.pickUp(ticket), bag);
     }
+
+    @Test
+    void should_store_in_first_locker_and_get_ticket_when_store_given_two_not_full_lockers() {
+        Locker firstLocker = new Locker(1);
+        RobotManager robotManager = new RobotManager(Arrays.asList(), Arrays.asList(firstLocker, new Locker(1)));
+
+        Bag bag = new Bag();
+        Ticket ticket = robotManager.store(bag);
+        assertEquals(firstLocker.pickUp(ticket), bag);
+    }
+
 }
